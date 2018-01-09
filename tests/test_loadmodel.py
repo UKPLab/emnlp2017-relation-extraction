3f625cba16811b0b9885d745eb780395daa21929
Daniil Sorokin <sorokin@ukp.informatik.tu-darmstadt.de>
2018-01-09 18:27:28 +0100

    Add tests

---
+++ tests/test_loadmodel.py
@@ -0,0 +1,24 @@
+import pytest
+
+from core import parser, entity_extraction
+
+
+def test_load_relationparser():
+    relparser = parser.RelParser("model_ContextWeighted",
+                          models_foldes="../trainedmodels/",
+                          resource_folder="../resources/",
+                          embeddings_location="../../resources/embeddings/glove/glove.6B.50d.txt")
+    tagged = [('Star', 'O', 'NNP'), ('Wars', 'O', 'NNP'), ('VII', 'O', 'NNP'), ('is', 'O', 'VBZ'), ('an', 'O', 'DT'),
+              ('American', 'MISC', 'JJ'), ('space', 'O', 'NN'), ('opera', 'O', 'NN'), ('epic', 'O', 'NN'),
+              ('film', 'O', 'NN'), ('directed', 'O', 'VBN'), ('by', 'O', 'IN'), ('J.', 'PERSON', 'NNP'),
+              ('J.', 'PERSON', 'NNP'), ('Abrams', 'PERSON', 'NNP'), ('.', 'O', '.')]
+    entity_fragments = entity_extraction.extract_entities(tagged)
+    edges = entity_extraction.generate_edges(entity_fragments)
+    non_parsed_graph = {'tokens': [t for t, _, _ in tagged],
+                    'edgeSet': edges}
+    graph_with_relations = relparser.sem_parse(non_parsed_graph)
+    assert [e['kbID'] for e in graph_with_relations['edgeSet']] == ['P800', 'P136', 'P136']
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
